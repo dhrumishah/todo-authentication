1876775602505478769f1f1653dd3b055d778f79
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2023-06-11 15:10:52 +0530

    error handling

--- app.js
+++ app.js
@@ -65,12 +65,20 @@ import userRouter from "./routes/user.js";
 import taskRouter from "./routes/task.js";
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors";
 
 export const app = express();
 
 // Using middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    credentials: true,
+  })
+);
 //using routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tasks", taskRouter);
